182fef9fd886d713f64e06cc5d986719b42ef241
doyeon326 <doyeon326@gmail.com>
2020-09-16 21:28:21 +0900

    Basic Netflix

--- MyNetflix/MyNetflix/HomeViewController.swift
+++ MyNetflix/MyNetflix/HomeViewController.swift
@@ -41,18 +41,26 @@ class HomeViewController: UIViewController {
     
 
     @IBAction func playButtonTapped(_ sender: Any) {
-//        SearchAPI.search("interstella") { movies in
-//            guard let interstella = movies.first else { return }
-//            DispatchQueue.main.async {
-//                let url = URL(string: interstella.previewURL)!
-//                let item = AVPlayerItem(url: url)
-//                
-//                let sb = UIStoryboard(name: "Player", bundle: nil)
-//                let vc = sb.instantiateViewController(identifier: "PlayerViewController") as! PlayerViewController
-//                vc.modalPresentationStyle = .fullScreen
-//                vc.player.replaceCurrentItem(with: item)
-//                self.present(vc, animated: false, completion: nil)
-//            }
-//        }
+        // interstella 에대한 정보를 검색API를 통해서 가져온다
+        // 거기서 인터스텔라 객체 한개를 가져온다
+        // 그 객체를 이용해서 PlayerViewController를 띄운다
+        // 인터스텔라 객체가 주소가 없기때문에 배트맨으로 대체
+        SearchAPI.search("Batman") { movies in
+            guard let interstella = movies.first else { return }
+            DispatchQueue.main.async {
+                let url = URL(string: interstella.previewURL)!
+                let item = AVPlayerItem(url: url)
+                let sb = UIStoryboard(name: "Player", bundle: nil)
+                let vc = sb.instantiateViewController(identifier: "PlayerViewController") as! PlayerViewController
+                vc.modalPresentationStyle = .fullScreen
+                vc.player.replaceCurrentItem(with: item)
+                self.present(vc, animated: false, completion: nil)
+            }
+        }
+        
+        
     }
 }
+
+
+
