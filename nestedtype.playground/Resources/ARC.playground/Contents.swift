ce1969491b6d1fd1a6c3c8bec2b159f9febbc35b
doyeon <doyeon326@gmail.com>
2020-12-22 23:00:52 +0900

    playground practices

---
+++ nestedtype.playground/Resources/ARC.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+class Person {
+    let name: String
+    init(name: String) {
+        self.name = name
+        print("\(name) is being initialized")
+    }
+    deinit {
+        print("\(name) is being deinitialized")
+    }
+}
+var reference1: Person?
+var reference2: Person?
+var reference3: Person?
+
+reference1 = Person(name: "John Appleseed")
+// Prints "John Appleseed is being initialized"
