6b563c4ecac1b44ecaf0eee310be730b5c0f3c11
doyeon326 <doyeon326@gmail.com>
2021-08-18 17:31:46 +0900

    AVSpeechSynthesizer

---
+++ AVSpeechSynthesizer/AVSpeechSynthesizer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AVSpeechSynthesizer
+//
+//  Created by doyeona on 2021/08/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
